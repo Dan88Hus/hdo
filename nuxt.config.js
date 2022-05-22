08d167274bf16400fe441d0ac3dc37da9ce46266
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2022-05-22 13:02:49 +0300

    server/index.js added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,5 @@
-import colors from 'vuetify/es5/util/colors'
 
-export default {
+module.exports = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - hdo',
@@ -44,22 +43,14 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
       }
     }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  telemetry: false,
 }
---
+++ server/index.js
@@ -0,0 +1,64 @@
+
+const express = require('express')
+const app = express()
+var cors = require('cors');
+const { readdirSync } = require("fs");
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+
+// Then use it before your routes are set up:
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+consola.ready({
+    message: `here`,
+    badge: true
+  })
+
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const { host, port } = nuxt.options.server
+
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
+
+// for test purposes
+app.get('/test', (req,res)=>{
+    res.send('test is OK')
+})
+
+
+//routes import 
+// routes middleware
+
+var normalizedPath = require("path").join(__dirname, "routes");
+
+// files in routes folder for rest API 
+readdirSync(normalizedPath).forEach((file) => app.use("/api", require("./routes/" + file)) )
