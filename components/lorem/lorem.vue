b85781cbe89da5f80049233a69710bd5765ab9b7
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2022-06-26 20:35:02 +0300

    some text bolded

--- components/lorem/lorem.vue
+++ components/lorem/lorem.vue
@@ -4,8 +4,7 @@
     <strong>HDO</strong> is an decoration service company:
     <br>
     <br>
-    We bring nature into your company in a very special and highly representative way and with a plant wall we ensure that your entrance area, your lobby, your location, your rooms become an experience and your customers and employees are enthusiastic. Our plant walls underline the architecture, create an interplay between modern steel, concrete and glass with nature and ensure the well-being of visitors and employees. Green wall gardens regulate humidity and enrich the air with oxygen, they have a sound-absorbing effect and create a calming, natural indoor climate that can reduce stress and health problems.
-
+    We bring nature into your company in a very special and highly representative way and with a plant wall we ensure that your <b>entrance area, your lobby, your location, your rooms</b> become an experience and your customers and employees are enthusiastic. Our plant walls underline the architecture, create an interplay between modern steel, concrete and glass with nature and ensure the well-being of visitors and employees. Green wall gardens regulate humidity and enrich the air with oxygen, they have a sound-absorbing effect and create a calming, natural <b>indoor</b> climate that can reduce stress and health problems.
     <br>
   </div>
 </template>
